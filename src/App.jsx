d071441f25a2071c2bffaf2b14fc27529828b42c
Azizbek:04 <150643944+az1zbek2004@users.noreply.github.com>
2024-04-12 22:11:58 +0500

    biroz o'zgarishlar kiritildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import "./App.css";
+import React, { useEffect, useState } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import Layout from "./loyaut/Layout";
 import Signup from "./pages/Signup";
@@ -9,14 +8,17 @@ import Movie from "./pages/Movie";
 import Series from "./pages/Series";
 import Bookmarks from "./pages/Bookmarks";
 import About from "./pages/About";
-function ProtectedRoute({children, redirectTo='/login', isAuthenticated}){
+
+function ProtectedRoute({ children, redirectTo = '/login', isAuthenticated }) {
   const navigate = useNavigate();
 
-  if (!isAuthenticated) {
-    navigate(redirectTo);
-  }
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate(redirectTo);
+    }
+  }, [isAuthenticated, navigate, redirectTo]);
 
-  return children
+  return isAuthenticated ? children : null;
 }
 
 function App() {
@@ -27,7 +29,7 @@ function App() {
     const storedToken = localStorage.getItem("token");
 
     if (!storedToken) {
-      setToken(null);// Redirect to login if no token found
+      setToken(false);// Redirect to login if no token found
     } else {
       setToken(storedToken);
     }
@@ -36,40 +38,52 @@ function App() {
   return (
     <>
       <Routes>
-        {/* Signup va Login yo'lovchilari token bo'yicha boshqa shartlar bilan ko'rsatilgan */}
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        
-        {/* Asosiy sahifa uchun Layout komponenti bilan birga ko'rsatilgan yo'lovchilar */}
-        <Route path="/" element={<Layout setSearch={setSearch} />}>
-          <Route index element={
-            <ProtectedRoute isAuthenticated={token?true:false}>
-              <Home search={search} />
-            </ProtectedRoute>
-          } />
-          <Route path="movie" element={
-            <ProtectedRoute isAuthenticated={token?true:false}>
-            <Movie search={search} />
-          </ProtectedRoute>
-          } />
-          <Route path="series" element={
-            <ProtectedRoute isAuthenticated={token?true:false}>
-              <Series search={search} />
-            </ProtectedRoute>
-          } />
-          <Route path="bookmarks" element={
-            <ProtectedRoute isAuthenticated={token?true:false}>
-              <Bookmarks search={search} />
-            </ProtectedRoute>
-          } />
+
+        <Route element={<Layout setSearch={setSearch} />}>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute redirectTo="/login" isAuthenticated={token ? true : false}>
+                <Home search={search} />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="movie"
+            element={
+              <ProtectedRoute redirectTo="/login" isAuthenticated={token ? true : false}>
+                <Movie search={search} />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="series"
+            element={
+              <ProtectedRoute redirectTo="/login" isAuthenticated={token ? true : false}>
+                <Series search={search} />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="bookmarks"
+            element={
+              <ProtectedRoute redirectTo="/login" isAuthenticated={token ? true : false}>
+                <Bookmarks search={search} />
+              </ProtectedRoute>
+            }
+          />
         </Route>
 
-        {/* About sahifasi */}
-        <Route path="/about" element={
-          <ProtectedRoute isAuthenticated={token?true:false}>
-            <About search={search} />
-          </ProtectedRoute>
-        } />
+        <Route
+          path="/about"
+          element={
+            <ProtectedRoute redirectTo="/login" isAuthenticated={token ? true : false}>
+              <About search={search} />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </>
   );
